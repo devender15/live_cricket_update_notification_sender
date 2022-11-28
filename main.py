45c18423b7d4eb8ef6d469a23b3d588191ffea42
Devender Pal <57867128+devender15@users.noreply.github.com>
2022-11-28 17:25:10 +0530

    added new functionality and overall optimized and enhanced the code

--- main.py
+++ main.py
@@ -1,199 +1,179 @@
 import requests
 from bs4 import BeautifulSoup
-from plyer import notification
-import time
-from win32com.client import Dispatch
+from lxml import etree, html
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+def send_discord(title, text, color):
+    WH_URL = "https://discord.com/api/webhooks/934155161469861958/9m_Sdcvxwb-kCAkYFTLDtbe79BSPawq3L6CqYnB-J0uq3hadqUeVEaX0zYlMx3m2owlC"
+
+    wh = DiscordWebhook(url=WH_URL)
+    embed = DiscordEmbed(title=title, description=text, color=color)
+    wh.add_embed(embed=embed)
+    wh.execute()
+
+# send_discord("wicket", "he got him!")
 
 
 def get_website(url):
-	r = requests.get(url)
-	return r.text
-
-def notify_me(title, message):
-	notification.notify(
-		title = title,
-		message = message,
-		timeout = 10
-	)
-
-def speak(str):
-	speak = Dispatch("SAPI.SpVoice")
-	speak.Speak(str)
+    r = requests.get(url)
+    return r.text
 
 
 def select_match():
-	home_page = get_website("https://www.espncricinfo.com/live-cricket-score")
+    home_page = get_website("https://www.espncricinfo.com/live-cricket-score")
 
-	soup = BeautifulSoup(home_page, "html.parser")
+    soup = BeautifulSoup(home_page, "html.parser")
 
-	match_links = []
-	match_names = []
+    match_links = []  # this array will store the match links
+    match_names = []
 
-	for all_matches in soup.find("div", class_="no-gutters").find_all("a", limit=2, class_="match-info-link-FIXTURES"):
-		match_links.append(all_matches['href'])
+    for all_matches in soup.findAll("a", class_="ds-no-tap-higlight"):
+        # print(all_matches.get_text())
+        match_links.append(all_matches.get("href"))
 
-	# getting all the match names
-	first_match = soup.find("div").find_all("span", class_="category")[5]
-	match_names.append(first_match.get_text())
+    # getting all the match names
 
+    for i in range(len(soup.find_all("span", class_="ds-underline"))):
+        match_name = soup.find_all("span", class_="ds-underline")[i]
+        # print(match_name.get_text())
+        match_names.append(match_name.get_text())
 
-	second_match = soup.find_all("div", class_="col-md-8")[1].find("div").find_all("span", class_="category")[5]
-	match_names.append(second_match.get_text())
+    # print(match_names)
 
-	complete_data = dict(zip(match_names, match_links))
+    complete_data = dict(zip(match_names, match_links))
+    # print(complete_data)
+    return complete_data
 
-	return complete_data
 
 def score_checker(url):
-	data = get_website(url)
+    data = get_website(url)
 
-	soup = BeautifulSoup(data, "html.parser")
+    soup = BeautifulSoup(data, "html.parser")
 
-	# info about current match
-	status_red = soup.find("div", class_="match-info-MATCH").find("div", class_="description")
+    tree = html.fromstring(data)
 
-	# latest event i.e 4, 6 or Wicket
-	latest_event = soup.find("div", class_="match-comment-run")
-	# print(latest_event.get_text())
+    # info about current match
+    unnecessory_keywords = ["Match State: Innings Break"]
 
+    event_array = tree.xpath(f'//*[@id="{url}"]//text()')
 
-	team_one = soup.find("div", class_="match-info-MATCH").find("div", class_="teams").find("div", class_="name-detail").find("a").find("p")
+    # removing the non-required elements from the array because we only want overs and "W", "6" or "4" events.
+    for line in unnecessory_keywords:
+        if (line in event_array):
+            event_array.remove(line)
 
-	team_two = soup.find("div", class_="match-info-MATCH").find("div", class_="teams").find_all("div", class_="team")[1].find("div", class_="name-detail").find("a").find("p")
+    print(event_array)
+    # latest event i.e 4, 6 or Wicket
+    latest_event = event_array[1]
+    print(latest_event)
 
-	score_batting_one = soup.find("div", class_="match-info-MATCH").find("div", class_="teams").find("div", class_="score-detail")
+    team_one = soup.findAll("div", class_="ci-team-score")[-2]  # team1 score
+    team_two = soup.findAll("div", class_="ci-team-score")[-1]  # team2 score
 
-	score_batting_two = soup.find("div", class_="match-info-MATCH").find("div", class_="teams").find_all("div", class_="team")[1].find("div", class_="score-detail")
+    current_over = ""
+    over_val = ""
 
+    # getting the value completed after the over
+    try:
+        over = soup.find("div", class_="match-comment-run-col")
+        over_parsed = over.get_text().split(".")
+        current_over = over_parsed[0]
+        over_val = over.get_text()[:-1]
 
-	# getting over completed value
-	try:	
-		over = soup.find("div", class_="match-comment-run-col")
-		over_parsed = over.get_text().split(".")
-		current_over = over_parsed[0]
+    except Exception as not_started:
+        pass
 
-	except Exception as not_started:
-		pass
+    # formatting the batting team score with name
 
+    try:
+        bat1 = team_one.get_text()
+        bat2 = team_two.get_text()
 
-	# formatting the batting team score with name
+    except Exception as e:
+        # print(f"{team_two.get_text()} is bowling! ")
+        bat1 = "  "
+        bat2 = "  "
 
-	try:
-		bat1 = f"{team_one.get_text()}  :   {score_batting_one.get_text()}"
-		bat2 = f"{team_two.get_text()}  :   {score_batting_two.get_text()}"
-		# print(bat2)
-		pass
-	except Exception as e:
-		# print(f"{team_two.get_text()} is bowling! ")
-		bat1 = "  "
-		bat2 = "  "
+    # status_text = soup.find("div", class_="match-info-MATCH-full-width").find("div", class_="status-text")
 
-	status_text = soup.find("div", class_="match-info-MATCH").find("div", class_="status-text")
+    try:
+        return latest_event, bat1, bat2, current_over, over_val
 
-	try:	
-		return latest_event.get_text(), bat1, bat2, status_text.get_text(), current_over, status_red.get_text()
+    except Exception as q:
+        print(f"error: {q}")
+        return None
 
-	except Exception as q:
-		# print(f"error: {q}")
-		return None
 
 def start_updating(user_input):
 
-	choice = select_match()
+    choice = select_match()
 
-	# converting all keys of dictionary to a list
-	key_list = list(choice.keys())
-	match_1 = key_list[0]
-	match_2 = key_list[1]
+    # converting all keys of dictionary to a list
+    key_list = list(choice.keys())
+    match_1 = key_list[0]
+    match_2 = key_list[1]
 
-	# getting all respective links
-	link_1 = choice[match_1]
-	link_2 = choice[match_2]
+    # getting all respective links
+    link_1 = choice[match_1]
+    link_2 = choice[match_2]
 
+    if (user_input == 1):
 
-	if(user_input==1):
+        if (score_checker("https://www.espncricinfo.com" + link_1) == None):
+            print("Match is not LIVE, please come later.")
 
-		if(score_checker("https://www.espncricinfo.com" + link_1)==None):
-			print("Match is not LIVE, please come later.")
+        else:
+            event, score, score2, current_over_, over_val = score_checker(
+                "https://www.espncricinfo.com" + link_1)
 
-		else:
-			event, score, score2, status_text_, current_over_, info_match = score_checker("https://www.espncricinfo.com" + link_1)
+            print(
+                "Now Fourth Umpire is active, just listen to the updates and do your other work :) ")
 
+            # making this check for not repeating same notification again and again !
+            check = {}
 
-			print("\nMatch updating has been started...\n\n ")
-			print(info_match)
+            while (True):
+                event, score, score2, current_over_,  over_val = score_checker(
+                    "https://www.espncricinfo.com" + link_1)
 
-			while(True):
-				event, score, score2, status_text_, current_over_, info_match = score_checker("https://www.espncricinfo.com" + link_1)
+                # checking for 4, 6 or Wicket from latest event
+                if (event == "4"):
+                    if (over_val not in check):
+                        check[over_val] = True
+                        four = "4!  Its a FOUR ! "
+                        send_discord(four, f"{score}\n{score2}\n", "FFA500")
 
-				# checking for 4, 6 or Wicket from latest event
-				if(event=="4"):
-					four = "4!  Its a FOUR ! "
-					speak("FOUR Runs !")
-					notify_me(four, f"{score}\n{score2}\n{status_text_}")
-					time.sleep(40)
+                elif (event == "6"):
+                    if (over_val not in check):
+                        check[over_val] = True
+                        six = "6!  Boom!  SIX !"
+                        send_discord(six, f"{score}\n{score2}\n", "00FF00")
 
-				elif(event=="6"):
-					six = "6!  Boom!  SIX !"
-					speak("Maximum! Its a SIX !")
-					notify_me(six, f"{score}\n{score2}\n{status_text_}")
-					time.sleep(40)
+                elif (event == "W"):
+                    if (over_val not in check):
+                        print("wicket")
+                        check[over_val] = True
+                        wicket = "Wicket! He got him"
+                        send_discord(wicket, f"{score}\n{score2}\n", "FF0000")
 
-				elif(event=="W"):
-					wicket = "Wicket! He got him"
-					speak("Out! Bowler got his wicket!")
-					notify_me(wicket, f"{score}\n{score2}\n{status_text_}")
-					time.sleep(80)
+                else:
+                    pass
 
-			print("Match is Ended !")
+            print("Match is Ended !")
 
-	elif(_input==2):
+    else:
+        print("Please select an appropriate option !")
 
-		if(score_checker("https://www.espncricinfo.com" + link_2)==None):
-			print("Match is not LIVE, please come later.")
-
-		else:
-			event, score, score2, status_text_, current_over_, info_match = score_checker("https://www.espncricinfo.com" + link_2)
-
-
-			print("Now Fourth Umpire is active, just listen to the updates and do your other work :) ")
-			print(info_match)
-
-			while(True):
-				event, score, score2, status_text_, current_over_, info_match = score_checker("https://www.espncricinfo.com" + link_2)
-
-				# checking for 4, 6 or Wicket from latest event
-				if(event=="4"):
-					four = "4!  Its a FOUR ! "
-					speak("FOUR Runs !")
-					notify_me(four, f"{score}\n{score2}\n{status_text_}")
-					time.sleep(40)
-
-				elif(event=="6"):
-					six = "6!  Boom! Its a SIX !"
-					speak("Maximum! Its a SIX !")
-					notify_me(six, f"{score}\n{score2}\n{status_text_}")
-					time.sleep(40)
-
-				elif(event=="W"):
-					wicket = "Wicket! He got him"
-					speak("Out! Bowler got his wicket!")
-					notify_me(wicket, f"{score}\n{score2}\n{status_text_}")
-					time.sleep(80)
-
-			print("Match is Ended !")
-
-	else:
-		print("Please select an appropriate option !")
 
 if __name__ == '__main__':
 
-	choice = select_match()
-	print("Top two matches are :\n")
+    choice = select_match()
+    print("Available LIVE matches :\n")
 
-	for live in choice:
-		print(live)
+    for live in choice:
+        print(live)
 
-	_input = int(input("\nSelect any match you want to get update:\n"))
+    _input = int(input("\nSelect any match you want to get update:\n"))
 
-	start_updating(_input)
+    start_updating(_input)
